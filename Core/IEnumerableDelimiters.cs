3ebbad6f333cd5d9490fcd9e78cce90e69d0108f
Jan Michael Sanchez Sierra <maicol_931028@hotmail.com>
2021-11-18 19:26:09 -0500

    Project creation and delimiter method

---
+++ Core/IEnumerableDelimiters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core
+{
+    class IEnumerableDelimiters
+    {
+        public IEnumerable<string> ListaPalabras(string palabras)
+        {
+            return palabras.Split(' ', '.', ',').Where(st => !st.Equals(""));
+        }
+    }
+}
